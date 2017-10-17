a870e5b9e3e82c1ba222ea4541cd8a02daa3f832
sami <sami.lanu@tut.fi>
2017-10-17 11:12:56 +0300

    Add UTF8 and -replace special characters in win_services.ps1

--- facts.d/win_services.ps1
+++ facts.d/win_services.ps1
@@ -8,4 +8,5 @@
 # ---------
 #
 # Copyright 2017 Tampere University of Technology
-get-service | select DisplayName, Status | ForEach-Object {$factName = $_.DisplayName; $status = $_.Status; Write-Host winservice_$factName"="$status}
+$OutPutEncoding = [ System.Text.Encoding]::UTF8
+get-service | select DisplayName, Status | ForEach-Object {$factName = $_.DisplayName -replace '([^a-z0-9)])', ''; $status = $_.Status; Write-Host winservice_$factName"="$status}
